cad810f35d9228884bd8b679e79e84f2a266ffe6
MigMax <maxence.martin.info@gmail.com>
2025-01-12 15:55:24 +0100

    Extract Discount computation from CartItem to Product

--- csharp/SupermarketReceipt/CartItem.cs
+++ csharp/SupermarketReceipt/CartItem.cs
@@ -7,9 +7,6 @@ public class CartItem(Product product, double quantity)
     public Product Product { get; } = product;
     public double Quantity { get; private set; } = quantity;
     
-    private static readonly CultureInfo Culture = CultureInfo.CreateSpecificCulture("en-GB");
-    
-
     public void AddQuantity(double quantity)
     {
         Quantity += quantity;
@@ -17,58 +14,6 @@ public class CartItem(Product product, double quantity)
     
     public Discount ComputeDiscount(Offer offer)
     {
-        var quantity = Quantity;
-
-        var quantityAsInt = (int)quantity;
-        
-        Discount discount = null;
-
-        var x = 1;
-
-        if (offer.OfferType == SpecialOfferType.ThreeForTwo)
-        {
-            x = 3;
-        }
-        else if (offer.OfferType == SpecialOfferType.TwoForAmount)
-        {
-            x = 2;
-
-            if (quantityAsInt >= 2)
-            {
-                var total = offer.Argument * (quantityAsInt / x) + quantityAsInt % 2 * Product.UnitPrice;
-                var discountN = Product.UnitPrice * quantity - total;
-                discount = new Discount(Product, "2 for " + PrintPrice(offer.Argument), -discountN);
-            }
-        }
-
-        if (offer.OfferType == SpecialOfferType.FiveForAmount) x = 5;
-
-        var numberOfXs = quantityAsInt / x;
-
-        switch (offer.OfferType)
-        {
-            case SpecialOfferType.ThreeForTwo when quantityAsInt > 2:
-            {
-                var discountAmount = quantity * Product.UnitPrice - (numberOfXs * 2 * Product.UnitPrice + quantityAsInt % 3 * Product.UnitPrice);
-                discount = new Discount(Product, "3 for 2", -discountAmount);
-                break;
-            }
-            case SpecialOfferType.TenPercentDiscount:
-                discount = new Discount(Product, offer.Argument + "% off", -quantity * Product.UnitPrice * offer.Argument / 100.0);
-                break;
-            case SpecialOfferType.FiveForAmount when quantityAsInt >= 5:
-            {
-                var discountTotal = Product.UnitPrice * quantity - (offer.Argument * numberOfXs + quantityAsInt % 5 * Product.UnitPrice);
-                discount = new Discount(Product, x + " for " + PrintPrice(offer.Argument), -discountTotal);
-                break;
-            }
-        }
-
-        return discount;
-    }
-    
-    private string PrintPrice(double price)
-    {
-        return price.ToString("N2", Culture);
+        return Product.GetDiscount(offer, Quantity);
     }
 }
--- csharp/SupermarketReceipt/Product.cs
+++ csharp/SupermarketReceipt/Product.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace SupermarketReceipt;
 
@@ -7,7 +8,64 @@ public class Product(string name, ProductUnit unit, double price)
     public string Name { get; } = name;
     public ProductUnit Unit { get; } = unit;
     public double UnitPrice { get; } = price;
+    
+    private static readonly CultureInfo Culture = CultureInfo.CreateSpecificCulture("en-GB");
 
+    public Discount GetDiscount(Offer offer, double quantity)
+    {
+        var quantityAsInt = (int)quantity;
+        
+        Discount discount = null;
+
+        var x = 1;
+
+        if (offer.OfferType == SpecialOfferType.ThreeForTwo)
+        {
+            x = 3;
+        }
+        else if (offer.OfferType == SpecialOfferType.TwoForAmount)
+        {
+            x = 2;
+
+            if (quantityAsInt >= 2)
+            {
+                var total = offer.Argument * (quantityAsInt / x) + quantityAsInt % 2 * UnitPrice;
+                var discountN = UnitPrice * quantity - total;
+                discount = new Discount(this, "2 for " + PrintPrice(offer.Argument), -discountN);
+            }
+        }
+
+        if (offer.OfferType == SpecialOfferType.FiveForAmount) x = 5;
+
+        var numberOfXs = quantityAsInt / x;
+
+        switch (offer.OfferType)
+        {
+            case SpecialOfferType.ThreeForTwo when quantityAsInt > 2:
+            {
+                var discountAmount = quantity * UnitPrice - (numberOfXs * 2 * UnitPrice + quantityAsInt % 3 * UnitPrice);
+                discount = new Discount(this, "3 for 2", -discountAmount);
+                break;
+            }
+            case SpecialOfferType.TenPercentDiscount:
+                discount = new Discount(this, offer.Argument + "% off", -quantity * UnitPrice * offer.Argument / 100.0);
+                break;
+            case SpecialOfferType.FiveForAmount when quantityAsInt >= 5:
+            {
+                var discountTotal = UnitPrice * quantity - (offer.Argument * numberOfXs + quantityAsInt % 5 * UnitPrice);
+                discount = new Discount(this, x + " for " + PrintPrice(offer.Argument), -discountTotal);
+                break;
+            }
+        }
+
+        return discount;
+    }
+    
+    private string PrintPrice(double price)
+    {
+        return price.ToString("N2", Culture);
+    }
+    
     public override bool Equals(object obj)
     {
         var product = obj as Product;
